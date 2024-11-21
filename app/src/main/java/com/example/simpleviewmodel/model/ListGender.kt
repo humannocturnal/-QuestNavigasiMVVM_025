b912b9b68f9eb9f2c6c2f024e43900132c956e91
humannocturnal <syaputrar374@gmail.com>
2024-11-21 16:47:24 +0700

    first commit

---
+++ app/src/main/java/com/example/simpleviewmodel/model/ListGender.kt
@@ -0,0 +1,11 @@
+package com.example.simpleviewmodel.model
+
+import android.content.Context
+import com.example.simpleviewmodel.R
+
+class ListGender(context: Context) {
+    val ListGender = listOf(
+        context.getString(R.string.laki),
+        context.getString(R.string.wanita)
+    )
+}
